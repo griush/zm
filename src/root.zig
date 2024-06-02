58ea0499945face5bbb9e08c89311f95226288bd
griush <gerardriush@gmail.com>
2024-06-02 15:39:46 +0200

    reworked api again

--- src/root.zig
+++ src/root.zig
@@ -1,109 +1,225 @@
-const std = @import("std");
-
 const RealType = f32;
 
-/// Vec2 type
-pub const Vec2 = @Vector(2, RealType);
+pub const Vec2 = struct {
+    const Self = @This();
 
-/// Vec3 type
-pub const Vec3 = @Vector(3, RealType);
+    data: @Vector(2, RealType),
 
-/// Vec4 type
-pub const Vec4 = @Vector(4, RealType);
+    pub fn from(in_x: RealType, in_y: RealType) Self {
+        return .{
+            .data = .{ in_x, in_y },
+        };
+    }
 
-/// Mat4 type
-pub const Mat4 = @Vector(16, RealType);
+    pub fn x(self: Self) RealType {
+        return self.data[0];
+    }
 
-pub fn scaleVec2(v: *Vec2, s: RealType) void {
-    v.* *= @splat(s);
-}
+    pub fn y(self: Self) RealType {
+        return self.data[1];
+    }
 
-pub fn scaleVec3(v: *Vec3, s: RealType) void {
-    v.* *= @splat(s);
-}
+    pub fn add(l: Self, r: Self) Self {
+        return Self{
+            .data = l.data + r.data,
+        };
+    }
 
-pub fn scaleVec4(v: *Vec4, s: RealType) void {
-    v.* *= @splat(s);
-}
+    pub fn neg(self: Self) Self {
+        return Self{
+            .data = -self.data,
+        };
+    }
 
-pub fn scaleMat4(m: *Mat4, s: RealType) void {
-    m.* *= @splat(s);
-}
+    pub fn scale(self: *Self, s: RealType) void {
+        self.data *= @splat(s);
+    }
 
-pub fn squareLengthVec2(v: Vec2) RealType {
-    return v[0] * v[0] + v[1] * v[1];
-}
+    pub fn squareLength(self: Self) RealType {
+        return self.x() * self.x() + self.y() * self.y();
+    }
 
-pub fn squareLengthVec3(v: Vec3) RealType {
-    return v[0] * v[0] + v[1] * v[1] + v[2] * v[2];
-}
+    pub fn length(self: Self) RealType {
+        return @sqrt(self.squareLength());
+    }
 
-pub fn squareLengthVec4(v: Vec4) RealType {
-    return v[0] * v[0] + v[1] * v[1] + v[2] * v[2] + v[3] * v[3];
-}
+    pub fn normalize(self: *Self) void {
+        self.scale(1 / self.length());
+    }
 
-pub fn lengthVec2(v: Vec2) RealType {
-    return @sqrt(squareLengthVec2(v));
-}
+    pub fn dot(l: Vec2, r: Vec2) RealType {
+        return l.x() * r.x() + l.y() * r.y() + l.z() * r.z();
+    }
+};
 
-pub fn lengthVec3(v: Vec3) RealType {
-    return @sqrt(squareLengthVec3(v));
-}
+pub const Vec3 = struct {
+    const Self = @This();
 
-pub fn lengthVec4(v: Vec4) RealType {
-    return @sqrt(squareLengthVec4(v));
-}
+    data: @Vector(3, RealType),
 
-pub fn normalizeVec2(v: *Vec2) void {
-    scaleVec2(v, 1 / lengthVec2(v.*));
-}
+    pub fn from(in_x: RealType, in_y: RealType, in_z: RealType) Self {
+        return .{
+            .data = .{ in_x, in_y, in_z },
+        };
+    }
 
-pub fn normalizeVec3(v: *Vec3) void {
-    scaleVec3(v, 1 / lengthVec3(v.*));
-}
+    pub fn x(self: Self) RealType {
+        return self.data[0];
+    }
 
-pub fn normalizeVec4(v: *Vec4) void {
-    scaleVec4(v, 1 / lengthVec4(v.*));
-}
+    pub fn y(self: Self) RealType {
+        return self.data[1];
+    }
 
-pub fn dotVec2(l: Vec2, r: Vec2) RealType {
-    return l[0] * r[0] + l[1] * r[1];
-}
+    pub fn z(self: Self) RealType {
+        return self.data[2];
+    }
 
-pub fn dotVec3(l: Vec3, r: Vec3) RealType {
-    return l[0] * r[0] + l[1] * r[1] + l[2] * r[2];
-}
+    pub fn add(l: Self, r: Self) Self {
+        return Self{
+            .data = l.data + r.data,
+        };
+    }
 
-pub fn dotVec4(l: Vec4, r: Vec4) RealType {
-    return l[0] * r[0] + l[1] * r[1] + l[2] * r[2] + l[3] * r[3];
-}
+    pub fn neg(self: Self) Self {
+        return Self{
+            .data = -self.data,
+        };
+    }
 
-pub fn cross(l: Vec3, r: Vec3) Vec3 {
-    return .{
-        l[1] * r[2] - l[2] * r[1],
-        l[2] * r[0] - l[0] * r[2],
-        l[0] * r[1] - l[1] * r[0],
-    };
-}
+    pub fn scale(self: *Self, s: RealType) void {
+        self.data *= @splat(s);
+    }
 
-pub fn diagonalMat4(r: RealType) Mat4 {
-    return Mat4{
-        r, 0, 0, 0,
-        0, r, 0, 0,
-        0, 0, r, 0,
-        0, 0, 0, r,
-    };
-}
+    pub fn squareLength(self: Self) RealType {
+        return self.x() * self.x() + self.y() * self.y() + self.z() * self.z();
+    }
 
-pub fn identity() Mat4 {
-    return diagonalMat4(1.0);
-}
+    pub fn length(self: Self) RealType {
+        return @sqrt(self.squareLength());
+    }
 
-pub fn orthographic(l: RealType, r: RealType, t: RealType, b: RealType, n: RealType, f: RealType) Mat4 {
-    return Mat4{
-        2 / (r - l), 0,           0,            -(r + l) / (r - l),
-        0,           2 / (t - b), 0,            -(t + b) / (t - b),
-        0,           0,           -2 / (f - n), -(f + n) / (f - n),
-        0,           0,           0,            1.0,
-    };
-}
+    pub fn normalize(self: *Self) void {
+        self.scale(1 / self.length());
+    }
+
+    pub fn dot(l: Vec3, r: Vec3) RealType {
+        return l.x() * r.x() + l.y() * r.y() + l.z() * r.z();
+    }
+
+    pub fn cross(l: Vec3, r: Vec3) Vec3 {
+        return .{
+            l.y() * r.z() - l.z() * r.y(),
+            l.z() * r.x() - l.x() * r.z(),
+            l.x() * r.y() - l.y() * r.x(),
+        };
+    }
+};
+
+pub const Vec4 = struct {
+    const Self = @This();
+
+    data: @Vector(4, RealType),
+
+    pub fn from(in_x: RealType, in_y: RealType, in_z: RealType, in_w: RealType) Self {
+        return .{
+            .data = .{ in_x, in_y, in_z, in_w },
+        };
+    }
+
+    pub fn x(self: Self) RealType {
+        return self.data[0];
+    }
+
+    pub fn y(self: Self) RealType {
+        return self.data[1];
+    }
+
+    pub fn z(self: Self) RealType {
+        return self.data[2];
+    }
+
+    pub fn w(self: Self) RealType {
+        return self.data[3];
+    }
+
+    pub fn add(l: Self, r: Self) Self {
+        return Self{
+            .data = l.data + r.data,
+        };
+    }
+
+    pub fn neg(self: Self) Self {
+        return Self{
+            .data = -self.data,
+        };
+    }
+
+    pub fn scale(self: *Self, s: RealType) void {
+        self.data *= @splat(s);
+    }
+
+    pub fn squareLength(self: Self) RealType {
+        return self.x() * self.x() + self.y() * self.y() + self.z() * self.z() + self.w() * self.w();
+    }
+
+    pub fn length(self: Self) RealType {
+        return @sqrt(self.squareLength());
+    }
+
+    pub fn normalize(self: *Self) void {
+        self.scale(1 / self.length());
+    }
+
+    pub fn dot(l: Vec4, r: Vec4) RealType {
+        return l.x() * r.x() + l.y() * r.y() + l.z() * r.z() + l.w() * r.w();
+    }
+};
+
+pub const Mat4 = struct {
+    const Self = @This();
+
+    data: @Vector(16, RealType),
+
+    pub fn diagonal(r: RealType) Mat4 {
+        return Mat4{
+            .data = .{
+                r, 0, 0, 0,
+                0, r, 0, 0,
+                0, 0, r, 0,
+                0, 0, 0, r,
+            },
+        };
+    }
+
+    pub fn identity() Mat4 {
+        return Mat4.diagonal(1.0);
+    }
+
+    pub fn orthographic(l: RealType, r: RealType, t: RealType, b: RealType, n: RealType, f: RealType) Mat4 {
+        return Mat4{
+            .data = .{
+                2 / (r - l), 0,           0,            -(r + l) / (r - l),
+                0,           2 / (t - b), 0,            -(t + b) / (t - b),
+                0,           0,           -2 / (f - n), -(f + n) / (f - n),
+                0,           0,           0,            1.0,
+            },
+        };
+    }
+
+    pub fn add(l: Self, r: Self) Self {
+        return Self{
+            .data = l.data + r.data,
+        };
+    }
+
+    pub fn neg(self: Self) Self {
+        return Self{
+            .data = -self.data,
+        };
+    }
+
+    pub fn scale(self: *Self, s: RealType) void {
+        self.data *= @splat(s);
+    }
+};
--- src/tests.zig
+++ src/tests.zig
@@ -2,75 +2,76 @@ const std = @import("std");
 const amth = @import("amth");
 
 test "Vec initialization" {
-    const v = amth.Vec2{ 2, -1 };
-    const v2 = amth.Vec3{ 1.5, -2, 4.2 };
+    const v = amth.Vec2.from(2, -1);
+    const v2 = amth.Vec3.from(1.5, -2, 4.2);
 
-    try std.testing.expectApproxEqAbs(2, v[0], 0.0001);
-    try std.testing.expectApproxEqAbs(-1, v[1], 0.0001);
+    try std.testing.expectApproxEqAbs(2, v.x(), 0.0001);
+    try std.testing.expectApproxEqAbs(-1, v.y(), 0.0001);
 
-    try std.testing.expectApproxEqAbs(1.5, v2[0], 0.0001);
-    try std.testing.expectApproxEqAbs(-2.0, v2[1], 0.0001);
-    try std.testing.expectApproxEqAbs(4.2, v2[2], 0.0001);
+    try std.testing.expectApproxEqAbs(1.5, v2.x(), 0.0001);
+    try std.testing.expectApproxEqAbs(-2.0, v2.y(), 0.0001);
+    try std.testing.expectApproxEqAbs(4.2, v2.z(), 0.0001);
 }
 
 test "vec arithmetic" {
-    const v = amth.Vec2{ 2, -1 };
-    const v2 = amth.Vec2{ 1.5, 4.2 };
+    const v = amth.Vec2.from(2, -1);
+    const v2 = amth.Vec2.from(1.5, 4.2);
 
-    const sum = v + v2;
-    const diff = v - v2;
+    // TODO: Find a nicer API for this
+    const sum = amth.Vec2.add(v, v2);
+    const diff = amth.Vec2.add(v, v2.neg());
 
-    try std.testing.expectApproxEqAbs(3.5, sum[0], 0.0001);
-    try std.testing.expectApproxEqAbs(3.2, sum[1], 0.0001);
+    try std.testing.expectApproxEqAbs(3.5, sum.x(), 0.0001);
+    try std.testing.expectApproxEqAbs(3.2, sum.y(), 0.0001);
 
-    try std.testing.expectApproxEqAbs(0.5, diff[0], 0.0001);
-    try std.testing.expectApproxEqAbs(-5.2, diff[1], 0.0001);
+    try std.testing.expectApproxEqAbs(0.5, diff.x(), 0.0001);
+    try std.testing.expectApproxEqAbs(-5.2, diff.y(), 0.0001);
 }
 
 test "Vec2 scale" {
-    var v = amth.Vec2{ 2, -1 };
+    var v = amth.Vec2.from(2, -1);
 
-    amth.scaleVec2(&v, 3);
+    v.scale(3.0);
 
-    try std.testing.expectApproxEqAbs(6, v[0], 0.0001);
-    try std.testing.expectApproxEqAbs(-3, v[1], 0.0001);
+    try std.testing.expectApproxEqAbs(6, v.x(), 0.0001);
+    try std.testing.expectApproxEqAbs(-3, v.y(), 0.0001);
 }
 
 test "Vec length" {
-    const v = amth.Vec2{ 2, -1 };
-    const len = amth.lengthVec2(v);
-    const square_len = amth.squareLengthVec2(v);
+    const v = amth.Vec2.from(2, -1);
+    const len = v.length();
+    const square_len = v.squareLength();
 
     try std.testing.expectApproxEqAbs(@sqrt(5.0), len, 0.0001);
     try std.testing.expectApproxEqAbs(5.0, square_len, 0.0001);
 }
 
 test "Vec dot" {
-    const v = amth.Vec3{ 1, 2, 3 };
-    const v2 = amth.Vec3{ 4, 5, 6 };
+    const v = amth.Vec3.from(1, 2, 3);
+    const v2 = amth.Vec3.from(4, 5, 6);
 
-    const dot = amth.dotVec3(v, v2);
+    const dot = amth.Vec3.dot(v, v2);
 
     try std.testing.expectApproxEqAbs(32.0, dot, 0.0001);
 }
 
 test "Vec normalize" {
-    var x = amth.Vec3{ 1, 0, 0 };
-    var y = amth.Vec3{ 0, 1, 0 };
-    var z = amth.Vec3{ 0, 0, 1 };
+    var x = amth.Vec3.from(1, 0, 0);
+    var y = amth.Vec3.from(0, 1, 0);
+    var z = amth.Vec3.from(0, 0, 1);
 
-    amth.normalizeVec3(&x);
-    amth.normalizeVec3(&y);
-    amth.normalizeVec3(&z);
+    x.normalize();
+    y.normalize();
+    z.normalize();
 
-    try std.testing.expectEqual(amth.Vec3{ 1, 0, 0 }, x);
-    try std.testing.expectEqual(amth.Vec3{ 0, 1, 0 }, y);
-    try std.testing.expectEqual(amth.Vec3{ 0, 0, 1 }, z);
+    try std.testing.expectEqual(@Vector(3, f32){ 1, 0, 0 }, x.data);
+    try std.testing.expectEqual(@Vector(3, f32){ 0, 1, 0 }, y.data);
+    try std.testing.expectEqual(@Vector(3, f32){ 0, 0, 1 }, z.data);
 }
 
 test "Mat4 scale" {
-    var identity = amth.identity();
-    amth.scaleMat4(&identity, 3.0);
+    var identity = amth.Mat4.identity();
+    identity.scale(3.0);
 
-    try std.testing.expectEqual(amth.diagonalMat4(3.0), identity);
+    try std.testing.expectEqual(amth.Mat4.diagonal(3.0).data, identity.data);
 }
