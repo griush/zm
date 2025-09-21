8e092db125fa958996f6e3274a7031397984b62e
Gerard Rius <gerardriush@gmail.com>
2025-09-21 20:32:57 +0200

    demo: add vector demo
    fixed ci as well

--- examples/demo.zig
+++ examples/demo.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 const zm = @import("zm");
 
 pub fn main() !void {
+    std.debug.print("zm demo\n\n", .{});
+
     std.debug.print("easing and animations\n", .{});
     var val: f32 = zm.ease(0, 100, 0.25, zm.EaseType.ease_in_out);
     std.debug.print("  ease_in_out: 0 -> 100 at 25% = {d}\n", .{val});
@@ -9,4 +11,18 @@ pub fn main() !void {
     std.debug.print("  ease_in_out: 0 -> 100 at 50% = {d}\n", .{val});
     val = zm.ease(0, 100, 0.75, zm.EaseType.ease_in_out);
     std.debug.print("  ease_in_out: 0 -> 100 at 75% = {d}\n", .{val});
+
+    std.debug.print("vectors\n", .{});
+    const v1 = zm.Vec(3, f32){ .data = .{ 1.0, 1.0, 0.0 } };
+    std.debug.print("  v1 = ({d},{d},{d})\n", .{v1.data[0], v1.data[1], v1.data[2]});
+    std.debug.print("  v1.len() = {d}\n", .{v1.len()});
+    const v1_norm = v1.norm();
+    std.debug.print("  v1.norm = ({d},{d},{d})\n", .{v1_norm.data[0], v1_norm.data[1], v1_norm.data[2]});
+
+    const right = zm.Vec3{ .data = .{ 1.0, 0.0, 0.0} };
+    const up = zm.Vec3{ .data = .{ 0.0, 1.0, 0.0} };
+    const up_x_rightRH = right.crossRH(up);
+    std.debug.print("  right x up RH = ({d},{d},{d})\n", .{up_x_rightRH.data[0], up_x_rightRH.data[1], up_x_rightRH.data[2]});
+    const up_x_rightLH = right.crossLH(up);
+    std.debug.print("  right x up LH = ({d},{d},{d})\n", .{up_x_rightLH.data[0], up_x_rightLH.data[1], up_x_rightLH.data[2]});
 }
--- src/matrix.zig
+++ src/matrix.zig
@@ -249,6 +249,71 @@ pub fn Mat(r: comptime_int, c: comptime_int, comptime T: type) type {
             return inv;
         }
 
+        pub fn fastInverse(self: Self) Self {
+            const m = self.data;
+
+            const t0  = m[2][2] * m[3][3];
+            const t1  = m[3][2] * m[2][3];
+            const t2  = m[1][2] * m[3][3];
+            const t3  = m[3][2] * m[1][3];
+            const t4  = m[1][2] * m[2][3];
+            const t5  = m[2][2] * m[1][3];
+            const t6  = m[0][2] * m[3][3];
+            const t7  = m[3][2] * m[0][3];
+            const t8  = m[0][2] * m[2][3];
+            const t9  = m[2][2] * m[0][3];
+            const t10 = m[0][2] * m[1][3];
+            const t11 = m[1][2] * m[0][3];
+            const t12 = m[2][0] * m[3][1];
+            const t13 = m[3][0] * m[2][1];
+            const t14 = m[1][0] * m[3][1];
+            const t15 = m[3][0] * m[1][1];
+            const t16 = m[1][0] * m[2][1];
+            const t17 = m[2][0] * m[1][1];
+            const t18 = m[0][0] * m[3][1];
+            const t19 = m[3][0] * m[0][1];
+            const t20 = m[0][0] * m[2][1];
+            const t21 = m[2][0] * m[0][1];
+            const t22 = m[0][0] * m[1][1];
+            const t23 = m[1][0] * m[0][1];
+
+            var result = Self.diagonal(0.0);
+
+            result.data[0][0] = (t0 * m[1][1] + t3 * m[2][1] + t4 * m[3][1]) - (t1 * m[1][1] + t2 * m[2][1] + t5 * m[3][1]);
+            result.data[0][1] = (t1 * m[0][1] + t6 * m[2][1] + t9 * m[3][1]) - (t0 * m[0][1] + t7 * m[2][1] + t8 * m[3][1]);
+            result.data[0][2] = (t2 * m[0][1] + t7 * m[1][1] + t10 * m[3][1]) - (t3 * m[0][1] + t6 * m[1][1] + t11 * m[3][1]);
+            result.data[0][3] = (t5 * m[0][1] + t8 * m[1][1] + t11 * m[2][1]) - (t4 * m[0][1] + t9 * m[1][1] + t10 * m[2][1]);
+
+            const d = 1.0 / (
+                m[0][0] * result.data[0][0] +
+                m[1][0] * result.data[0][1] +
+                m[2][0] * result.data[0][2] +
+                m[3][0] * result.data[0][3]
+            );
+
+            result.data[0][0] *= d;
+            result.data[0][1] *= d;
+            result.data[0][2] *= d;
+            result.data[0][3] *= d;
+
+            result.data[1][0] = d * ((t1 * m[1][0] + t2 * m[2][0] + t5 * m[3][0]) - (t0 * m[1][0] + t3 * m[2][0] + t4 * m[3][0]));
+            result.data[1][1] = d * ((t0 * m[0][0] + t7 * m[2][0] + t8 * m[3][0]) - (t1 * m[0][0] + t6 * m[2][0] + t9 * m[3][0]));
+            result.data[1][2] = d * ((t3 * m[0][0] + t6 * m[1][0] + t11 * m[3][0]) - (t2 * m[0][0] + t7 * m[1][0] + t10 * m[3][0]));
+            result.data[1][3] = d * ((t4 * m[0][0] + t9 * m[1][0] + t10 * m[2][0]) - (t5 * m[0][0] + t8 * m[1][0] + t11 * m[2][0]));
+
+            result.data[2][0] = d * ((t12 * m[1][3] + t15 * m[2][3] + t16 * m[3][3]) - (t13 * m[1][3] + t14 * m[2][3] + t17 * m[3][3]));
+            result.data[2][1] = d * ((t13 * m[0][3] + t18 * m[2][3] + t21 * m[3][3]) - (t12 * m[0][3] + t19 * m[2][3] + t20 * m[3][3]));
+            result.data[2][2] = d * ((t14 * m[0][3] + t19 * m[1][3] + t22 * m[3][3]) - (t15 * m[0][3] + t18 * m[1][3] + t23 * m[3][3]));
+            result.data[2][3] = d * ((t17 * m[0][3] + t20 * m[1][3] + t23 * m[2][3]) - (t16 * m[0][3] + t21 * m[1][3] + t22 * m[2][3]));
+
+            result.data[3][0] = d * ((t14 * m[2][2] + t17 * m[3][2] + t13 * m[1][2]) - (t16 * m[3][2] + t12 * m[1][2] + t15 * m[2][2]));
+            result.data[3][1] = d * ((t20 * m[3][2] + t12 * m[0][2] + t19 * m[2][2]) - (t18 * m[2][2] + t21 * m[3][2] + t13 * m[0][2]));
+            result.data[3][2] = d * ((t18 * m[1][2] + t23 * m[3][2] + t15 * m[0][2]) - (t22 * m[3][2] + t14 * m[0][2] + t19 * m[1][2]));
+            result.data[3][3] = d * ((t22 * m[2][2] + t16 * m[0][2] + t21 * m[1][2]) - (t20 * m[1][2] + t23 * m[2][2] + t17 * m[0][2]));
+
+            return result;
+        }
+
         pub fn fromQuaternion(q: Quaternion(T)) Self {
             comptime {
                 if (r != 4 or c != 4) {
