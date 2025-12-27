b8684258de10b0a18a79d80e6406da2d110935df
Theo <theo.morales.fr@gmail.com>
2025-12-27 22:56:46 +0100

    Add vec mul and mulAssign

--- src/vec.zig
+++ src/vec.zig
@@ -74,6 +74,20 @@ pub fn Vec(n: comptime_int, comptime T: type) type {
             return sum;
         }
 
+        pub fn mul(self: Self, other: Self) Self {
+            var result: Self = .{ .data = undefined };
+            inline for (0..n) |i| {
+                result.data[i] = self.data[i] * other.data[i];
+            }
+            return result;
+        }
+
+        pub fn mulAssign(self: *Self, other: Self) void {
+            inline for (0..n) |i| {
+                self.data[i] *= other.data[i];
+            }
+        }
+
         pub fn lenSq(self: Self) T {
             return self.dot(self);
         }
